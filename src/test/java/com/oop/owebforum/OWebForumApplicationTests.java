a807af02235b017ee4cca7b754e53586cabe8526
MaximPavliuk1 <maksym.pavliuk.shi.2023@lpnu.ua>
2024-05-22 22:03:12 +0300

    Initial commit

---
+++ src/test/java/com/oop/owebforum/OWebForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oop.owebforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OWebForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
